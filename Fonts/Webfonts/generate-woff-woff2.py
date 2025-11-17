c16eb4517f38af876810ffd9a1ceb6b697a19ed4
Cristiano Sobral <cssobral2013@gmail.com>
2025-11-17 00:49:48 -0300

    Update (Metropolis New)

---
+++ Fonts/Webfonts/generate-woff-woff2.py
@@ -0,0 +1,333 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = '../TrueType/MetropolisNew-Thin.ttf'
+woffPath = './WOFF/MetropolisNew-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = '../TrueType/MetropolisNew-ThinItalic.ttf'
+woffPath = './WOFF/MetropolisNew-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = '../TrueType/MetropolisNew-ExtraLight.ttf'
+woffPath = './WOFF/MetropolisNew-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = '../TrueType/MetropolisNew-ExtraLightItalic.ttf'
+woffPath = './WOFF/MetropolisNew-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = '../TrueType/MetropolisNew-Light.ttf'
+woffPath = './WOFF/MetropolisNew-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-LightItalic.ttf'
+woffPath = './WOFF/MetropolisNew-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-LightItalic.ttf'
+woffPath = './WOFF/MetropolisNew-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Regular.ttf'
+woffPath = './WOFF/MetropolisNew-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Italic.ttf'
+woffPath = './WOFF/MetropolisNew-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Medium.ttf'
+woffPath = './WOFF/MetropolisNew-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-MediumItalic.ttf'
+woffPath = './WOFF/MetropolisNew-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-SemiBold.ttf'
+woffPath = './WOFF/MetropolisNew-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-SemiBoldItalic.ttf'
+woffPath = './WOFF/MetropolisNew-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-SemiBold.ttf'
+woffPath = './WOFF/MetropolisNew-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-SemiBoldItalic.ttf'
+woffPath = './WOFF/MetropolisNew-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Bold.ttf'
+woffPath = './WOFF/MetropolisNew-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-BoldItalic.ttf'
+woffPath = './WOFF/MetropolisNew-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-ExtraBold.ttf'
+woffPath = './WOFF/MetropolisNew-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-ExtraBoldItalic.ttf'
+woffPath = './WOFF/MetropolisNew-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Black.ttf'
+woffPath = './WOFF/MetropolisNew-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-BlackItalic.ttf'
+woffPath = './WOFF/MetropolisNew-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = '../TrueType/MetropolisNew-Thin.ttf'
+woff2Path = './WOFF2/MetropolisNew-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = '../TrueType/MetropolisNew-ThinItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = '../TrueType/MetropolisNew-ExtraLight.ttf'
+woff2Path = './WOFF2/MetropolisNew-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = '../TrueType/MetropolisNew-ExtraLightItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = '../TrueType/MetropolisNew-Light.ttf'
+woff2Path = './WOFF2/MetropolisNew-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-LightItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-LightItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Regular.ttf'
+woff2Path = './WOFF2/MetropolisNew-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Italic.ttf'
+woff2Path = './WOFF2/MetropolisNew-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Medium.ttf'
+woff2Path = './WOFF2/MetropolisNew-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-MediumItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-SemiBold.ttf'
+woff2Path = './WOFF2/MetropolisNew-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-SemiBoldItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-SemiBold.ttf'
+woff2Path = './WOFF2/MetropolisNew-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-SemiBoldItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Bold.ttf'
+woff2Path = './WOFF2/MetropolisNew-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-BoldItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-ExtraBold.ttf'
+woff2Path = './WOFF2/MetropolisNew-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-ExtraBoldItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-Black.ttf'
+woff2Path = './WOFF2/MetropolisNew-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = '../TrueType/MetropolisNew-BlackItalic.ttf'
+woff2Path = './WOFF2/MetropolisNew-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
